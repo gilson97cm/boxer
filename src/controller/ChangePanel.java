ee1bb3067177ebaeed6d7a0ec20aa6cd6decb5ad
Code Mort <codemort2.47@gmail.com>
2020-03-02 23:06:25 -0500

    mi xommit

---
+++ src/controller/ChangePanel.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author RojeruSan
+ */
+import javax.swing.JPanel;
+
+public class ChangePanel {
+    //
+    private JPanel container;
+    private JPanel content;
+
+
+    /**
+     * Constructor de clase
+     */
+    public ChangePanel(JPanel container, JPanel content) {
+        this.container = container;
+        this.content = content;
+        this.container.removeAll();
+        this.container.revalidate();
+        this.container.repaint();
+        
+        this.container.add(this.content);
+        this.container.revalidate();
+        this.container.repaint();
+    }
+
+}//--> fin clase
